bbe0009c0cc08329c1667e93702e76ad1d607a54
Derek Walz <derek.walz@ucalgary.ca>
2023-03-04 11:24:49 -0700

    login and authorization

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,10 +3,12 @@ import {
 	getUsers,
 	createUser,
 	updateUser,
+    authUser
 } from '../controllers/userController.js';
 import { protect } from '../authMiddleware.js';
 const router = Router();
 
+router.get('/login', authUser);
 router.get('/', protect, getUsers);
 router.post('/', createUser);
 router.patch('/', updateUser);
