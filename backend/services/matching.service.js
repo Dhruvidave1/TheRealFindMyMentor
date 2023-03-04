fb15cebd42541322488d68b00164e26d4cffe5ba
Graeme Folk <graemefolk@gmail.com>
2023-03-04 12:25:13 -0700

    first working version of matching service

--- backend/services/matching.service.js
+++ backend/services/matching.service.js
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-import connectDB from "../config/db.js";
 import Profile from "../models/profileModel.js";
 
 const USER_CAN_MENTOR = {
@@ -10,14 +8,19 @@ const USER_CAN_MENTOR = {
   HCA: ["HCA"],
   Student: [],
 };
+const NUM_MATCHES_TO_RETURN = 10;
 
-dotenv.config();
-await connectDB();
-getMatches("6403781cb92f9eed2bdcc524");
+// import dotenv from "dotenv";
+// import connectDB from "../config/db.js";
+// import { exit } from "process";
+// dotenv.config();
+// await connectDB();
+// getMatches("6403781cb92f9eed2bdcc524");
+// exit();
 
 export async function getMatches(uid) {
   const user = await Profile.findById(uid).exec();
-  console.log(user);
+  //   console.log(user);
   if (!user) {
     console.log("User not found");
     return null;
@@ -29,6 +32,8 @@ export async function getMatches(uid) {
   if (user.isMentee) {
     matches.mentors = await getMentors(user);
   }
+  //   console.log("Number of mentors: " + matches.mentors?.length || 0);
+  //   console.log("Number of mentees: " + matches.mentees?.length || 0);
   return matches;
 }
 
@@ -39,12 +44,7 @@ async function getMentees(user) {
     "mentees"
   );
   const result = getBestMatches(user, potentialMatches);
-  console.log("Number of mentees: " + result.length);
-  console.log("Top 3 matches:");
-  for (let i = 0; i < 5; i++) {
-    console.log(result[i].matchScore, result[i]);
-  }
-  return result;
+  return result.slice(0, NUM_MATCHES_TO_RETURN);
 }
 
 async function getMentors(user) {
@@ -54,15 +54,9 @@ async function getMentors(user) {
     "mentors"
   );
   const result = getBestMatches(user, potentialMatches);
-
-  console.log("Number of mentors: " + result.length);
-  console.log("Top 3 matches:");
-  console.log(result.map((r) => r.matchScore));
-  //   for (let i = 0; i < 5; i++) {
-  //     console.log(result[i].matchScore, result[i]);
-  //   }
-
-  return result;
+  //   console.log("Number of mentors: " + result.length);
+  //   console.log(result.map((r) => r.matchScore));
+  return result.slice(0, NUM_MATCHES_TO_RETURN);
 
   function getEligibleMentorDesignations() {
     const mentorDesignations = [];
@@ -90,13 +84,13 @@ async function getAllEligibleUsers(allowedDesignations, userType) {
 function getBestMatches(user, matches) {
   const result = [...matches];
   for (const match of result) {
-    match.matchScore = genMatchScore(match);
+    match.matchScore = generateMatchScore(match);
   }
   return result
     .filter((match) => match.matchScore > 0)
     .sort((a, b) => b.matchScore - a.matchScore);
 
-  function genMatchScore(match) {
+  function generateMatchScore(match) {
     let result = 0;
     result += countMatches(user.zone, match.zone);
     if (user.isMentee) {
@@ -112,6 +106,7 @@ function getBestMatches(user, matches) {
 
   function countMatches(userField, matchField) {
     let result = 0;
+
     for (const item of userField) {
       if (matchField.includes(item)) {
         result++;
