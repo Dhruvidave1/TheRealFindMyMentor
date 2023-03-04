98f7ccf3cd1b82574921969d78b74ffac53a1d39
Derek Walz <derek.walz@ucalgary.ca>
2023-03-04 10:08:40 -0700

    complete routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userController = require('../controllers/userController');
 
+
+
 const router = express.Router();
 
-router.get("/", userController.getUsers);
+router.get("/", protect, userController.getUsers);
 router.post("/", userController.createUser);
 router.patch("/", userController.updateUser);
 router.delete("/". userController.removeUser);
