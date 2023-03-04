6c028da9887c62ae782e6c7140c31ee93f7fe824
David Cheng <32114983+ChengDave@users.noreply.github.com>
2023-03-04 12:14:53 -0700

    added match page and components

--- frontend/findmymentorapp/src/components/MatchProfile.js
+++ frontend/findmymentorapp/src/components/MatchProfile.js
@@ -1,42 +1,24 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import './MatchProfile.css';
+import Button from '@mui/material/Button';
 
-function MatchProfile({ person }) {
-  const {
-    firstName,
-    lastName,
-    email,
-    workLocation,
-    isMentor,
-    isMentee,
-    yearsOfPractice,
-    designation,
-    zone,
-    areasInterest,
-    mentorshipGoals
-  } = person;
-
+function MatchProfile(props) {
+  const { firstName, lastName, workLocation, designation, areasInterest, mentorshipGoals, image } = props.person;
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={person.image} />
-      <Card.Body>
-        <Card.Title>{firstName} {lastName}</Card.Title>
-        <Card.Text>
-          {`${yearsOfPractice} ${designation}, ${workLocation}`}
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Areas of Interest: {areasInterest.join(', ')}</ListGroup.Item>
-        <ListGroup.Item>Mentorship Goals: {mentorshipGoals.join(', ')}</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href={`mailto:${email}`}>Contact</Card.Link>
-        {isMentor && <Card.Text>Mentor</Card.Text>}
-        {isMentee && <Card.Text>Mentee</Card.Text>}
-        <Card.Text>Zone: {zone}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div>
+      <div className="match-profile">
+        <img src={image} alt={`${firstName} ${lastName}`} />
+        <h3>{firstName} {lastName}</h3>
+        <p>{workLocation}</p>
+        <p>{designation}</p>
+        <p><strong>Areas of Interest: </strong>{areasInterest.join(', ')}</p>
+        <p><strong>Mentorship Goals: </strong>{mentorshipGoals.join(', ')}</p>
+        </div>
+        <div className="button-container">
+          <Button variant="contained" className="like-button">Like</Button>
+          <Button variant="contained" className="dislike-button">Dislike</Button>
+        </div>
+    </div>
   );
 }
 
