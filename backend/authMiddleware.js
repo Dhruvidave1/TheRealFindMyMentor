d2728412e354b1c0e41795ce4b864632a77a45e3
Dhruvidave1 <dhruvi@ualberta.ca>
2023-03-04 13:42:12 -0700

    handled error

--- backend/authMiddleware.js
+++ backend/authMiddleware.js
@@ -11,22 +11,17 @@ const protect = asyncHandler(async (req, res, next) => {
 	) {
 		try {
 			token = req.headers.authorization.split(' ')[1];
-
 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
 			req.Profile = await Profile.findById(decoded.id).select('-password');
-
 			next();
 		} catch (error) {
 			console.error(error);
-			res.status(401);
-			throw new Error('Not authorized, token failed');
+			res.status(401).json({ error: `Not authorized: ${error.message}` });
 		}
 	}
 
 	if (!token) {
-		res.status(401);
-		throw new Error('Not authorized, no token');
+		res.status(401).json({ error: `Not authorized: ${error.message}` });
 	}
 });
 
