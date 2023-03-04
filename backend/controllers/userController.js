c7012804323320005034d9a11125e98d0bf97046
Dhruvidave1 <dhruvi@ualberta.ca>
2023-03-04 11:46:12 -0700

    added getUsers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,34 +1,35 @@
-import { generateToken } from "../utils/generateToken.js";
-import Profile from "../models/profileModel.js";
+import { generateToken } from '../utils/generateToken.js';
+import Profile from '../models/profileModel.js';
 
 export const getUsers = async (req, res) => {
-    console.log(req.Profile._id);
-	res.send({ success: true, data: 'getUsers!' });
+	const user = await Profile.findById(req.Profile._id);
+	res.send({
+		user,
+	});
 };
 
 export const authUser = async (req, res) => {
-    // data that is being sent from front end
-    const { email, password } = req.body
-    // finding a user whose email matches the email coming from front end
-    const user = await Profile.findOne({ email: email })
-  
-    if (user && (await user.matchPassword(password))) {
-      // if user exists, and password matches, we will send this data back
-      // token has user id embedded in it as the payload
-      res.json({
-        _id: user._id,
-        first_name: user.firstName,
-        last_name: user.lastName,
-        email: user.email,
-        token: generateToken(user._id),
-      })
-    } else {
-      res.status(401)
-      throw new Error('Invalid email or password')
-    }
-    res.send({ email, password })
-  }
+	// data that is being sent from front end
+	const { email, password } = req.body;
+	// finding a user whose email matches the email coming from front end
+	const user = await Profile.findOne({ email: email });
 
+	if (user && (await user.matchPassword(password))) {
+		// if user exists, and password matches, we will send this data back
+		// token has user id embedded in it as the payload
+		res.json({
+			_id: user._id,
+			first_name: user.firstName,
+			last_name: user.lastName,
+			email: user.email,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(401);
+		throw new Error('Invalid email or password');
+	}
+	res.send({ email, password });
+};
 
 export const createUser = async (req, res) => {
 	res.send({ success: true, data: 'createUsers!' });
