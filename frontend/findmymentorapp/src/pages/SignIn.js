742a554b409b65b304f5cbe7bbab429e39539813
Graeme Folk <graemefolk@gmail.com>
2023-03-04 15:50:25 -0700

    cleanup

--- frontend/findmymentorapp/src/pages/SignIn.js
+++ frontend/findmymentorapp/src/pages/SignIn.js
@@ -1,23 +1,17 @@
-import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useState, useContext } from "react";
-import { useSessionStorageState } from "../hooks/useSessionStorageState";
+import React, { useState, useContext } from "react";
 import { APIContext } from "../context/api-provider.js";
 import { useNavigate } from "react-router-dom";
-
-const API_URL = "http://localhost:4000/api/";
 
 const theme = createTheme();
 
