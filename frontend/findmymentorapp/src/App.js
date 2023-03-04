78904868b63f1c972bc53d0ef34f82e2aeaed373
Graeme Folk <graemefolk@gmail.com>
2023-03-04 16:25:13 -0700

    updated routes to use auth guarding

--- frontend/findmymentorapp/src/App.js
+++ frontend/findmymentorapp/src/App.js
@@ -1,34 +1,20 @@
-import './App.css';
-import Navbar from './components/NavBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LogIn from './pages/SignIn';
-import DashBoard from './pages/Dashboard.js';
-import Match from './pages/Match';
-import Signup from './pages/SignUp';
-import Summary from './pages/Summary';
-import ViewProfile from './pages/ViewProfile';
-import { APIProvider } from './context/api-provider';
+import "./App.css";
+import Navbar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { APIProvider } from "./context/api-provider";
+import CreateRoutes from "./services/CreateRoutes";
 
 function App() {
-
+  const routes = CreateRoutes();
 
   return (
     <>
-    <APIProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<LogIn />} />
-          <Route path='/DashBoard' element={<DashBoard />} />
-          <Route path='/Match' element={<Match />} />
-          <Route path='/SignIn' element={<LogIn />} /> 
-          <Route path='/Signup' element={<Signup />} />
-          <Route path='/Summary' element={<Summary />} />      
-          <Route path='/ViewProfile' element={<ViewProfile />} />
-          
-        </Routes>
-      </Router>
-    </APIProvider>
+      <APIProvider>
+        <Router>
+          <Navbar />
+          {routes}
+        </Router>
+      </APIProvider>
     </>
   );
 }
---
+++ frontend/findmymentorapp/src/services/CreateRoutes.js
@@ -0,0 +1,92 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { APIContext } from "../context/api-provider";
+import LogIn from "../pages/SignIn";
+import DashBoard from "../pages/Dashboard.js";
+import Match from "../pages/Match";
+import Signup from "../pages/SignUp";
+import Summary from "../pages/Summary";
+import ViewProfile from "../pages/ViewProfile";
+
+// check to ensure we are logged in before navigating to a private page
+function PrivateRoute({ children }) {
+  const { isLoggedIn } = useContext(APIContext);
+  return isLoggedIn() ? children : <Navigate to="/login" />;
+}
+
+// don't allow navigation to the certain pages if user is already authenticated
+function AnonymousRoute({ children }) {
+  const { isLoggedIn } = useContext(APIContext);
+  return isLoggedIn() ? <Navigate to="/" /> : children;
+}
+
+function CreateRoutes() {
+  return (
+    <Routes>
+      <Route
+        exact
+        path="/login"
+        element={
+          <AnonymousRoute>
+            <LogIn />
+          </AnonymousRoute>
+        }
+      />
+      <Route
+        exact
+        path="/signup"
+        element={
+          <AnonymousRoute>
+            <Signup />
+          </AnonymousRoute>
+        }
+      />
+      <Route
+        exact
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <DashBoard />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        exact
+        path="/match"
+        element={
+          <PrivateRoute>
+            <Match />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        exact
+        path="/summary"
+        element={
+          <PrivateRoute>
+            <Summary />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        exact
+        path="/viewprofile"
+        element={
+          <PrivateRoute>
+            <ViewProfile />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        exact
+        path="/"
+        element={
+          <PrivateRoute>
+            <DashBoard />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
+}
+export default CreateRoutes;
