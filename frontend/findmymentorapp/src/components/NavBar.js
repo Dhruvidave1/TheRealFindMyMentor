fd2dc4b15854e5efa62511c9687297068a1297e1
Graeme Folk <graemefolk@gmail.com>
2023-03-04 20:11:15 -0700

    re-implement navbar

--- frontend/findmymentorapp/src/components/NavBar.js
+++ frontend/findmymentorapp/src/components/NavBar.js
@@ -8,7 +8,7 @@ import {
   Toolbar,
   IconButton,
   Box,
-  Tooltip,
+  Container,
   Button,
   Menu,
   MenuItem,
@@ -24,121 +24,111 @@ function NavBar() {
   //   const { darkMode, setDarkMode } = useContext(DarkModeContext);
   const { isLoggedIn, logout } = useContext(APIContext);
 
-  const [anchorElUser, setAnchorElUser] = useState(null);
-  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const pages = isLoggedIn()
+    ? [
+        { label: "Matches", link: "/match" },
+        { label: "Profile", link: "/viewprofile" },
+      ]
+    : [];
 
-  const settings = [{ text: "Logout", action: logout }];
-  let pages;
-  if (isLoggedIn()) {
-    pages = ["Matches", "Profile"];
-  } else {
-    pages = ["Sign Up", "Login"];
-  }
+  const title = "FindMyMentor";
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <AppBar position="static" sx={{ mb: 4 }}>
-      <Toolbar style={{ justifyContent: "space-between" }}>
-        <Box sx={{ flexGrow: 0 }}>
-          {/* <Tooltip title="Toggle Theme">
-            <IconButton onClick={() => setDarkMode(!darkMode)}>
-              {darkMode ? <Brightness4 /> : <Brightness7 />}
-            </IconButton>
-          </Tooltip> */}
-          {isLoggedIn() && (
-            <Tooltip title="User Settings">
-              <IconButton onClick={handleOpenUserMenu}>
-                <AccountCircle />
-              </IconButton>
-            </Tooltip>
-          )}
-          <Menu
-            sx={{ mt: "45px" }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            {settings.map((setting) => (
-              <MenuItem key={setting.text} onClick={setting.action}>
-                <Typography textAlign="center">{setting.text}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
-        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleOpenNavMenu}
-            color="inherit"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "left",
-            }}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
             sx={{
-              display: { xs: "block", md: "none" },
+              mr: 2,
+              display: { xs: "none", md: "flex" },
             }}
           >
+            {title}
+          </Typography>
+
+          {isLoggedIn() && (
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <Link
+                    key={page.label}
+                    to={page.link}
+                    onClick={handleCloseNavMenu}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    <MenuItem>
+                      <Typography textAlign="center">{page.label}</Typography>
+                    </MenuItem>
+                  </Link>
+                ))}
+              </Menu>
+            </Box>
+          )}
+          <Typography
+            variant="h5"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+            }}
+          >
+            {title}
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <Link to={`/${page}`} className="nav-links">
-                {" "}
-                {page}{" "}
+              <Link
+                key={page.label}
+                to={page.link}
+                onClick={handleCloseNavMenu}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <Button sx={{ my: 2, color: "white", display: "block" }}>
+                  {page.label}
+                </Button>
               </Link>
             ))}
-          </Menu>
-        </Box>
-
-        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-          {pages.map((page) => (
-            <Button
-              key={page}
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              {page}
-            </Button>
-          ))}
-        </Box>
-      </Toolbar>
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
